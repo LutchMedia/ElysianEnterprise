8d1381a42908302fa86adc67c2a1c7fe9c769997
Jonah Lutchmedial <jonah.lutchmedial@rns.cc>
2015-07-11 17:48:31 -0300

    Ai for Kayden's survival game

---
+++ PlayerHealth.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var Health : int = 200;
+
+function Start () {
+
+}
+
+function Update () 
+{
+	if(Health <= 0)
+	{
+		Destroy(this.gameObject);
+	}
+}
+function ApplyDammage(TheDammage : int)
+{
+	Health -= TheDammage;
+}
